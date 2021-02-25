bf42ffbb83a319d9dbefc3733749db62a56fb22d
winmife <ximnetherpvp649@gmail.com>
2021-02-25 12:08:25 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
 const cat_btn = document.getElementById('cat_btn');
 const cat_result = document.getElementById('cat_result');
+
+
 cat_btn.addEventListener('click', getRandomCat);
+
+
 function getRandomCat() {
 	fetch('https://aws.random.cat/meow')
 		.then(res => res.json())
@@ -8,4 +12,6 @@ function getRandomCat() {
 			cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
 		});
 }
+
+
 
